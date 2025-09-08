5b7ab48c14be1c3161d5381c2caa67b3c4307800
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-08 22:31:57 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import NexusConvergenceMap4D from '../components/NexusConvergenceMap4D';
+
+export default function Home() {
+  return (
+    <main>
+      <NexusConvergenceMap4D />
+    </main>
+  );
+}
